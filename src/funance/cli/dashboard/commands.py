0f27c50f15f1c6edaff7b3c9646808ff50e70a18
Dan Bowen <dan@crucialwebstudio.com>
2022-06-20 16:58:47 -0600

    Add invest spec file

--- src/funance/cli/dashboard/commands.py
+++ src/funance/cli/dashboard/commands.py
@@ -8,15 +8,18 @@ from dateutil.relativedelta import relativedelta
 from funance.dashboard.dash_app import create_app
 from funance.forecast.datespec import DATE_FORMAT
 from funance.forecast.projector import Projector
-from funance.common.paths import FORECAST_DIST_FILE, FORECAST_FILE
+from funance.common.paths import FORECAST_DIST_FILE, FORECAST_FILE, INVEST_DIST_FILE, INVEST_FILE
 
 
 def get_watch_files():
-    return [FORECAST_DIST_FILE, FORECAST_FILE]
+    return [FORECAST_DIST_FILE, FORECAST_FILE, INVEST_DIST_FILE, INVEST_FILE]
 
 
-def get_yaml():
-    spec_file = FORECAST_FILE if os.path.exists(FORECAST_FILE) else FORECAST_DIST_FILE
+def get_yaml(type_):
+    if type_ == 'forecast':
+        spec_file = FORECAST_FILE if os.path.exists(FORECAST_FILE) else FORECAST_DIST_FILE
+    if type_ == 'invest':
+        spec_file = INVEST_FILE if os.path.exists(INVEST_FILE) else INVEST_DIST_FILE
     with open(spec_file, "r") as stream:
         return yaml.safe_load(stream)
 
@@ -32,12 +35,17 @@ def get_end_date(start_date):
 @click.command(short_help='Run the dashboard')
 def dashboard():
     """Open the dashboard"""
+
     start_date = get_start_date()
     end_date = get_end_date(start_date)
-    spec = get_yaml()
-    projector = Projector.from_spec(spec,
+    forecast_spec = get_yaml('forecast')
+    projector = Projector.from_spec(forecast_spec,
                                     start_date.strftime(DATE_FORMAT),
                                     end_date.strftime(DATE_FORMAT))
-    charts = projector.get_charts()
+    forecast_charts = projector.get_charts()
+
+    invest_charts = []
+
+    charts = forecast_charts + invest_charts
     app = create_app(*charts)
     app.run_server(debug=True, extra_files=get_watch_files())
