358ff0482e5b741f1ab9d71e65b6e14fa3ff49bf
Dan Bowen <dan@crucialwebstudio.com>
2022-07-13 17:30:12 -0600

    Rename

--- src/funance/cli/dashboard/commands.py
+++ src/funance/cli/dashboard/commands.py
@@ -14,7 +14,7 @@ def get_watch_files():
     return [SPEC_DIST_FILE, SPEC_FILE]
 
 
-def get_yaml():
+def get_spec():
     spec_file = SPEC_FILE if os.path.exists(SPEC_FILE) else SPEC_DIST_FILE
     with open(spec_file, "r") as stream:
         return yaml.safe_load(stream)
@@ -23,6 +23,6 @@ def get_yaml():
 @click.command(short_help='Run the dashboard')
 def dashboard():
     """Open the dashboard"""
-    spec = get_yaml()
+    spec = get_spec()
     app = create_app(spec)
     app.run_server(debug=True, extra_files=get_watch_files())
